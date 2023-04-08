6ed1eca01caa2206b8c5133a3a715c5e0be77fe9
HelloJon29 <jonkim1996@gmail.com>
2023-04-07 19:41:01 -0500

    fixed number of columns drawn

--- js_practice/draw_patterns/square.js
+++ js_practice/draw_patterns/square.js
@@ -2,7 +2,7 @@ let n = 5;
 let string = "";
 for (let i = 0; i < n; i++) {
   string = "*";
-  for (let j = 0; j < n; j++) {
+  for (let j = 0; j < n - 1; j++) {
     string += "*";
   }
   console.log(string);
